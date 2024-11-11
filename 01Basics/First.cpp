e4d12e38f529791eb1605711ebfdb226a4292e6e
bettercodevivek <viveksingh0520@gmail.com>
2024-11-11 20:58:48 +0530

    Updation

--- 01Basics/First.cpp
+++ 01Basics/First.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+using namespace std;
 int main(){
-    std :: cout << "Hello World \n" ;
-    return  0;
+      
+      int a,b;
+
+cout<<"Enter the values for a and b \n";
+
+ cin>>a;
+ cin>>b;
+
+ cout<<"The total value is => \n"<<a+b<<endl;       
+
+    //  cout << "Hello World \n" ;
 }
