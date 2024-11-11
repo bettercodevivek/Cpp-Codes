9a20613836564407ed9ed3da9c0e49856a2d47e7
bettercodevivek <viveksingh0520@gmail.com>
2024-11-11 21:09:22 +0530

    learning to create custom namespace in cpp

--- 01Basics/First.cpp
+++ 01Basics/First.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
+
+// we can write our own namespace also in the following manner 
+
+namespace hello {
+      int display(){
+           int a=25;
+           int b=35;
+       int sum = a+b;
+            return sum;
+      }
+}
+
+
 int main(){
       
+// using our custom namespace in the code
+
+ int value=hello::display();
+
       int a,b;
 
 cout<<"Enter the values for a and b \n";
@@ -9,7 +26,10 @@ cout<<"Enter the values for a and b \n";
  cin>>a;
  cin>>b;
 
- cout<<"The total value is => \n"<<a+b<<endl;       
+ cout<<"The total value is => \n"<<a+b<<endl;  
+
+
+ cout << "Value recieved from the custom namespace is => " << value;     
 
     //  cout << "Hello World \n" ;
 }
