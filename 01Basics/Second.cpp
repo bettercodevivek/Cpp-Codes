c3e0657356dc75e20a380d4016fb1ed856f0b67f
bettercodevivek <viveksingh0520@gmail.com>
2024-11-11 22:28:22 +0530

    Learning about variable and constants

---
+++ 01Basics/Second.cpp
@@ -0,0 +1,21 @@
+// In this we will learn about variables,constants,datatypes.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int var1 ; // This is declaration of a variable
+
+var1=20; // This is initialisation of that variable
+
+int var2=25; // We can do both in a single line like this;
+
+// once declared variables do not need to be declared and their values can be modified too.
+
+// But if you want the value to remain same and that i should not be modified we can use const keyword
+
+const int var3 = 60;
+
+
+}
