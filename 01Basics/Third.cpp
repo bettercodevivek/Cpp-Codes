e53f7291cdddb2dfe814f64ff670bcfb319250f8
bettercodevivek <viveksingh0520@gmail.com>
2024-11-13 23:01:00 +0530

    Second task on operators completed

--- 01Basics/Third.cpp
+++ 01Basics/Third.cpp
@@ -4,18 +4,32 @@ int main(){
     // We'll performn some tasks to learn about operators
     // Task 1
 
-    int NumberOfCups ;
-    int PricePerCup ;
+    // int NumberOfCups ;
+    // int PricePerCup ;
 
-    cout<<"Enter the number of cups you want \n";
-    cin>>NumberOfCups;
+    // cout<<"Enter the number of cups you want \n";
+    // cin>>NumberOfCups;
 
-    cout<<"Now enter the price per cup\n";
-    cin>>PricePerCup;
+    // cout<<"Now enter the price per cup\n";
+    // cin>>PricePerCup;
 
-    int totalVal = PricePerCup * NumberOfCups ;
+    // int totalVal = PricePerCup * NumberOfCups ;
 
-    cout<< " The total value of all the cups is :- "<<totalVal<<endl;
+    // cout<< " The total value of all the cups is :- "<<totalVal<<endl;
+
+    
+
+    // Task 2
+
+    int NumberOfTeabags ;
+    cout<<"Enter the number of teabags\n";
+    cin>>NumberOfTeabags;
+
+    if(NumberOfTeabags<10){
+        NumberOfTeabags += 5 ;
+    }
+
+    cout<<NumberOfTeabags;
 
     return 0;
 }
