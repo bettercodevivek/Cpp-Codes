ec149e7414725dcb7bcff7437ccd24bf96f35d84
bettercodevivek <viveksingh0520@gmail.com>
2024-11-23 14:01:20 +0530

    Logical Operators Assignment

--- 01Basics/Third.cpp
+++ 01Basics/Third.cpp
@@ -34,23 +34,41 @@ int main(){
     
     // Task 3
 
-    int NumberOfTeaCups;
-    cout<<"Enter the Number of tea cups you bought"<<endl;
-    cin>>NumberOfTeaCups;
-    string BadgeName;
+    // int NumberOfTeaCups;
+    // cout<<"Enter the Number of tea cups you bought"<<endl;
+    // cin>>NumberOfTeaCups;
+    // string BadgeName;
 
-    if(NumberOfTeaCups >= 20){
-        BadgeName="GOLD";
-        cout<<"The Badge you have recieved is :- "<<BadgeName;
-    }
-    else if(NumberOfTeaCups>10 && NumberOfTeaCups<20){
-        BadgeName="SILVER";
-        cout<<"The Badge you have recieved is :- "<<BadgeName;
-    }
-    else{
-        BadgeName="BRONZE";
-        cout<<"The Badge you have recieved is :- "<<BadgeName;
-    }
+    // if(NumberOfTeaCups >= 20){
+    //     BadgeName="GOLD";
+    //     cout<<"The Badge you have recieved is :- "<<BadgeName;
+    // }
+    // else if(NumberOfTeaCups>10 && NumberOfTeaCups<20){
+    //     BadgeName="SILVER";
+    //     cout<<"The Badge you have recieved is :- "<<BadgeName;
+    // }
+    // else{
+    //     BadgeName="BRONZE";
+    //     cout<<"The Badge you have recieved is :- "<<BadgeName;
+    // }
+
+
+    // Task 4
+     
+     string status;
+     cout<<"Enter your status if you are a student or not :- "<<endl;
+     getline(cin,status);
+
+     int NumberOfCups;
+     cout<<"Enter the Number of cups you have"<<endl;
+     cin>>NumberOfCups;
+
+     if(status=="student" || NumberOfCups > 15){
+        cout<<"You are eligible for a discount"<<endl;
+     }
+     else{
+        cout<<"Not eligible for discount"<<endl;
+     }
 
 
 
