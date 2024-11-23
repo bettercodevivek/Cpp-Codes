d09e82091813536ad410d8c6a1055ae8ee5dff3d
bettercodevivek <viveksingh0520@gmail.com>
2024-11-23 19:03:48 +0530

    First task on while loop completed

--- 01Basics/Fourth.cpp
+++ 01Basics/Fourth.cpp
@@ -1,2 +1,20 @@
+#include<iostream>
+using namespace std;
 // Now we will perform tasks to practice loops
 
+// Task 1
+int main(){
+int Orders;
+int RemainingCups=Orders;
+int i=1;
+cout<<"enter the number of orders"<<endl;
+cin>>Orders;
+
+while(i <= Orders){
+   cout<<"The current tea cup being served is => "<<i<<endl;
+   RemainingCups=Orders-i;
+   cout<<"The number of remaining tea cups is => "<<RemainingCups<<endl;
+   i++;
+}
+ return 0;
+}                     
