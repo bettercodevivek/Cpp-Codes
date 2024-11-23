986d3fd4dbf564f4826604edc919a20eea08e446
bettercodevivek <viveksingh0520@gmail.com>
2024-11-23 23:08:46 +0530

    second pattern completed

--- 01Basics/Patterns.cpp
+++ 01Basics/Patterns.cpp
@@ -10,6 +10,16 @@ void pattern1(int n){
     }
 }
 
+void pattern2(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
     pattern1(5);
+    pattern2(10);
 }
