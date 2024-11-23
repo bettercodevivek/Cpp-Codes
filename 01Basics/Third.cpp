8546794bacd7a6fa2b604191d1301ba24df5ebb8
bettercodevivek <viveksingh0520@gmail.com>
2024-11-23 13:49:59 +0530

    Operators

--- 01Basics/Third.cpp
+++ 01Basics/Third.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 int main(){
     // We'll performn some tasks to learn about operators
@@ -21,15 +22,37 @@ int main(){
 
     // Task 2
 
-    int NumberOfTeabags ;
-    cout<<"Enter the number of teabags\n";
-    cin>>NumberOfTeabags;
+    // int NumberOfTeabags ;
+    // cout<<"Enter the number of teabags\n";
+    // cin>>NumberOfTeabags;
 
-    if(NumberOfTeabags<10){
-        NumberOfTeabags += 5 ;
+    // if(NumberOfTeabags<10){
+    //     NumberOfTeabags += 5 ;
+    // }
+
+    // cout<<NumberOfTeabags;
+    
+    // Task 3
+
+    int NumberOfTeaCups;
+    cout<<"Enter the Number of tea cups you bought"<<endl;
+    cin>>NumberOfTeaCups;
+    string BadgeName;
+
+    if(NumberOfTeaCups >= 20){
+        BadgeName="GOLD";
+        cout<<"The Badge you have recieved is :- "<<BadgeName;
+    }
+    else if(NumberOfTeaCups>10 && NumberOfTeaCups<20){
+        BadgeName="SILVER";
+        cout<<"The Badge you have recieved is :- "<<BadgeName;
+    }
+    else{
+        BadgeName="BRONZE";
+        cout<<"The Badge you have recieved is :- "<<BadgeName;
     }
 
-    cout<<NumberOfTeabags;
+
 
     return 0;
 }
