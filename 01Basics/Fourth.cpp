bb96b5051b63d7d2128ee5053cd3a7759851df93
bettercodevivek <viveksingh0520@gmail.com>
2024-11-23 19:09:02 +0530

    Task 2 on do while loop completed

--- 01Basics/Fourth.cpp
+++ 01Basics/Fourth.cpp
@@ -1,20 +1,34 @@
 #include<iostream>
 using namespace std;
-// Now we will perform tasks to practice loops
+
+int main(){
+    // Now we will perform tasks to practice loops
 
 // Task 1
-int main(){
-int Orders;
-int RemainingCups=Orders;
-int i=1;
-cout<<"enter the number of orders"<<endl;
-cin>>Orders;
 
-while(i <= Orders){
-   cout<<"The current tea cup being served is => "<<i<<endl;
-   RemainingCups=Orders-i;
-   cout<<"The number of remaining tea cups is => "<<RemainingCups<<endl;
-   i++;
+// int Orders;
+// int RemainingCups=Orders;
+// int i=1;
+// cout<<"enter the number of orders"<<endl;
+// cin>>Orders;
+
+// while(i <= Orders){
+//    cout<<"The current tea cup being served is => "<<i<<endl;
+//    RemainingCups=Orders-i;
+//    cout<<"The number of remaining tea cups is => "<<RemainingCups<<endl;
+//    i++;
+// }
+
+
+// Task 2
+
+string Choice;
+
+do{
+    cout<<"Hello My Good Sir, Do wish to have more tea?"<<endl;
+    getline(cin,Choice);
 }
+while(Choice!="no");
+
  return 0;
 }                     
