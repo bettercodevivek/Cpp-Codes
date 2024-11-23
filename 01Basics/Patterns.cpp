9b062b946e05b0d3607a43a60eb5135becb89ea1
bettercodevivek <viveksingh0520@gmail.com>
2024-11-23 23:18:48 +0530

    Pattern 4 & 5 completed

--- 01Basics/Patterns.cpp
+++ 01Basics/Patterns.cpp
@@ -28,8 +28,28 @@ void pattern3(int n){
     }
 }
 
+void pattern4(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
+
+void pattern5(int n){
+    for(int i=n;i>0;i--){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
     // pattern1(5);
     // pattern2(10);
-    pattern3(10);
+    // pattern3(10);
+    // pattern4(10);
+    pattern5(5);
 }
