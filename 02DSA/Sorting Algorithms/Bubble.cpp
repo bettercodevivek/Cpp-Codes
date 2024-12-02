c47168b2d5a658298703b09c5dae20e276038b36
bettercodevivek <viveksingh0520@gmail.com>
2024-12-02 19:14:48 +0530

    Bubble sort completed

--- 02DSA/Sorting Algorithms/Bubble.cpp
+++ 02DSA/Sorting Algorithms/Bubble.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
-    return 0;
+    int arr[5]={7,12,9,11,3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+
+    for(int i=n-1;i>=0;i--){
+        for(int j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
 }
