6c0aad27d396ba269115983c59917504ab5f11a2
bettercodevivek <viveksingh0520@gmail.com>
2024-12-02 13:20:36 +0530

    Reversing a vector

--- 02DSA/Basic Maths/Recursion.cpp
+++ 02DSA/Basic Maths/Recursion.cpp
@@ -2,6 +2,7 @@
 
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 void countdown(int n){
@@ -59,14 +60,47 @@ int SumOfNum(int n){
 //    return factorial;
 // }
 
-void ReverseArray(int n){
-    
-}
+// int ReverseArray(int n,int arr[]){
+//     for(int i=0, j = n-1;i<n && j>0;i++ && j--){
+//         int temp;
+//           arr[i]=arr[j];
+//           arr[j]=temp;
+//           temp=arr[i];
+//     }
+//     return arr;
+// }
 int main(){
     // countdown(100);
     // cout<<Factorial(5);
     // NamePrinter(10);
     // NumberPrinter(1,200);
     // NumberPrinter2(1,100);
- 
+//     int arr[5]={1,2,3,4,5};
+//    int i=0,j=5-1,temp;
+//    while(i<j){
+//       temp=arr[i];
+//       arr[i]=arr[j];
+//       arr[j]=temp;
+//       i++;
+//       j--;
+//    }
+//     for(int i=0;i<5;i++){
+//         cout<<arr[i]<<" ";
+//     }
+
+// vector<int> arr1={1,2,3,4,5,6,7,8,9,10};
+// auto itr1= arr1.begin();
+// auto itr2 = arr1.end()-1;
+// auto itr3=0;
+// while(itr1<itr2){
+//     itr3=*itr1;
+//     *itr1=*itr2;
+//     *itr2=itr3;
+//     itr1++;
+//     itr2--;
+// }
+// for(int val:arr1){
+//     cout<<val<<" ";
+// }
+
 }
