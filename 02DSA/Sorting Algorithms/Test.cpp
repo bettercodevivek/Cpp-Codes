ce0c0cacc47028784f911da3979a9d8c19f7b50d
bettercodevivek <viveksingh0520@gmail.com>
2024-12-05 15:46:13 +0530

    Implemented insertion sort

--- 02DSA/Sorting Algorithms/Test.cpp
+++ 02DSA/Sorting Algorithms/Test.cpp
@@ -24,11 +24,24 @@ void Bubble(int arr[],int n){
         }
     }
 }
+void Insertion(int arr[],int n){
+    for(int i=0;i<n;i++){
+       int j=i;
+       while(j>0 && arr[j-1]>arr[j]){
+        int temp=arr[j];
+        arr[j]=arr[j-1];
+        arr[j-1]=temp;
+        j--;
+       }
+        }
+    
+}
 int main(){
      
      int arr1[6]={5,9,2,1,12,4};
     //  selection(arr1,6);
-     Bubble(arr1,6);
+    //  Bubble(arr1,6);
+    Insertion(arr1,6);
      for(int i=0;i<6;i++){
         cout<<arr1[i]<<" ";
      }
