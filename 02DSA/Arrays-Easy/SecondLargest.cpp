4b040c75237204000871d0a1128fc5505f9cfbdb
bettercodevivek <viveksingh0520@gmail.com>
2024-12-07 14:24:00 +0530

    implemented optimal approach for second largest

--- 02DSA/Arrays-Easy/SecondLargest.cpp
+++ 02DSA/Arrays-Easy/SecondLargest.cpp
@@ -3,22 +3,43 @@
 using namespace std;
 int main(){
     vector<int> arr={12,233,234,56,61,34,345};
-    vector<int> n;
+// This one is a better approach than the brute force approach of sorting the array and simply returning second last and second element.
     int smallest=INT_MAX,largest=INT_MIN;
     int secondSmallest=INT_MAX,secondLargest=INT_MIN;
-    for(int i=0;i<arr.size();i++){
-       smallest=min(smallest,arr[i]);
-       largest=max(largest,arr[i]);
-    }
-    for(int i=0;i<arr.size();i++){
+    // for(int i=0;i<arr.size();i++){
+    //    smallest=min(smallest,arr[i]);
+    //    largest=max(largest,arr[i]);
+    // }
+    // for(int i=0;i<arr.size();i++){
         
-        if(arr[i]<secondSmallest && arr[i]!=smallest){
-            secondSmallest=arr[i];
-        }
-        if(arr[i]>secondLargest && arr[i]!=secondLargest){
-            secondLargest=arr[i];
-        }
+    //     if(arr[i]<secondSmallest && arr[i]!=smallest){
+    //         secondSmallest=arr[i];
+    //     }
+    //     if(arr[i]>secondLargest && arr[i]!=secondLargest){
+    //         secondLargest=arr[i];
+    //     }
 
-       }
-       cout<<secondLargest<<secondSmallest;
+    //    }
+    //    cout<<secondLargest<<secondSmallest;
+
+
+// let us now see a more optimal approach where we have to traverse the array only once.
+
+  for(int i=0;i<arr.size();i++){
+    if(arr[i]<smallest){
+        secondSmallest=smallest;
+        smallest=arr[i];
+    }
+    else if(arr[i]<secondSmallest && arr[i]!=smallest){
+        secondSmallest=arr[i];
+    }
+    if(arr[i]>largest){
+        secondLargest=largest;
+        largest=arr[i];
+    }
+    else if(arr[i]>secondLargest && arr[i]!=largest){
+        secondLargest=arr[i];
+    }
+  }
+    cout<<secondLargest<<endl<<secondSmallest;
     }
