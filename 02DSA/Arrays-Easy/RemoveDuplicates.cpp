dce30602b045c2890ecb45d7eb151693cd64f6c9
bettercodevivek <viveksingh0520@gmail.com>
2024-12-10 19:03:17 +0530

    Implemented RemoveDuplicate

--- 02DSA/Arrays-Easy/RemoveDuplicates.cpp
+++ 02DSA/Arrays-Easy/RemoveDuplicates.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+// Here, we are using two pointers i and j, where i is basically poiting towards the unique element whereas j iterates through the array and tells i about unique element.
+// after this, i increases 1 index and stores the unique element by overwriting on current value.
+
 int main(){
-    int arr[10]={0,0,1,1,1,2,2,3,3,4};
+    vector<int> nums = {1,1,2};
     int i=0;
-    for(int j=1;j<10;){
-       if(arr[j]==arr[i]){
-           j++;
-       }
-       else{
-        i++;
-        arr[i]=arr[j];
-       }
+    for(int j=1;j<nums.size();j++){
+         if(nums[j]!=nums[i]){
+            i++;
+             nums[i]=nums[j];
+         }
     }
-    cout<<i+1;
+    cout<<i+1<<endl;
 }
