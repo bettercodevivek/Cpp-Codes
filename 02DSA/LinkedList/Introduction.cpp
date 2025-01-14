ed94096610919b2019a1fc1738e6d10587cfe205
bettercodevivek <viveksingh0520@gmail.com>
2025-01-15 00:11:07 +0530

    Insertion learnt

--- 02DSA/LinkedList/Introduction.cpp
+++ 02DSA/LinkedList/Introduction.cpp
@@ -1,48 +1,55 @@
 #include<iostream>
 using namespace std;
-
 class Node{
     public:
-    int data;
-    Node* Next;
-
-    Node(int val){
-       data=val;
+   int data;
+   Node* Next;
+   Node(int value){
+       data=value;
        Next=NULL;
-    }
+   }
 };
 
 void insertAtEnd(Node*& head,int val){
     Node* node1=new Node(val);
-    if(head==NULL){
+    if(head == NULL){
         head=node1;
         return;
     }
-
     Node* temp=head;
-    while(temp->Next != NULL){
+    while(temp->Next!=NULL){
         temp=temp->Next;
     }
     temp->Next=node1;
 }
-
+void insertAtBegin(Node*& head,int val){
+    Node* node1=new Node(val);
+    
+    if(head==NULL){
+        head=node1;
+    }
+    else{
+        node1->Next=head;
+        head=node1;
+    }
+}
 void display(Node* head){
     Node* temp=head;
-
-    while(temp != NULL){
-        cout<<temp->data<<"  ";
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
         temp=temp->Next;
     }
     cout<<"NULL"<<endl;
 }
 
 int main(){
-    Node* head = NULL;
-
+    Node* head=NULL;
+    insertAtBegin(head,70);
     insertAtEnd(head,10);
     insertAtEnd(head,20);
     insertAtEnd(head,30);
     insertAtEnd(head,40);
-
+    insertAtBegin(head,60);
+    
     display(head);
 }
