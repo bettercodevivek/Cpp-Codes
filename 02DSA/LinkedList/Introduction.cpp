10f3c9a8128fb5270bd5860f15583603e18f43e0
bettercodevivek <viveksingh0520@gmail.com>
2025-02-04 20:33:28 +0530

    Updation

--- 02DSA/LinkedList/Introduction.cpp
+++ 02DSA/LinkedList/Introduction.cpp
@@ -1,55 +1,17 @@
-#include<iostream>
-using namespace std;
-class Node{
-    public:
-   int data;
-   Node* Next;
-   Node(int value){
-       data=value;
-       Next=NULL;
-   }
+// Linked List is a linear data structure like array, but it doesn't store values in contigous locations like array.
+
+// building block of LL is a node.
+
+// Each node contains two things :- 1=> data and 2=> pointer to next node
+
+// how to create a node for LL
+
+class Node {
+    int data;
+    Node* Next;
+
+    Node(int val){
+        data=val;
+        Next=nullptr;
+    }
 };
-
-void insertAtEnd(Node*& head,int val){
-    Node* node1=new Node(val);
-    if(head == NULL){
-        head=node1;
-        return;
-    }
-    Node* temp=head;
-    while(temp->Next!=NULL){
-        temp=temp->Next;
-    }
-    temp->Next=node1;
-}
-void insertAtBegin(Node*& head,int val){
-    Node* node1=new Node(val);
-    
-    if(head==NULL){
-        head=node1;
-    }
-    else{
-        node1->Next=head;
-        head=node1;
-    }
-}
-void display(Node* head){
-    Node* temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<"->";
-        temp=temp->Next;
-    }
-    cout<<"NULL"<<endl;
-}
-
-int main(){
-    Node* head=NULL;
-    insertAtBegin(head,70);
-    insertAtEnd(head,10);
-    insertAtEnd(head,20);
-    insertAtEnd(head,30);
-    insertAtEnd(head,40);
-    insertAtBegin(head,60);
-    
-    display(head);
-}
