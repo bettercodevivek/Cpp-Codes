2b346498212f955743f2e034aa58cea6b75f4c15
bettercodevivek <viveksingh0520@gmail.com>
2025-02-05 20:33:46 +0530

    Implemented conversion of an array to a singly linked list

--- 02DSA/LinkedList/Introduction.cpp
+++ 02DSA/LinkedList/Introduction.cpp
@@ -5,13 +5,38 @@
 // Each node contains two things :- 1=> data and 2=> pointer to next node
 
 // how to create a node for LL
-
+#include<bits/stdc++.h>
+using namespace std;
 class Node {
+    public:
     int data;
     Node* Next;
-
+    
+    public:
     Node(int val){
         data=val;
         Next=nullptr;
     }
 };
+
+Node* ArraytoLL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover=head;
+
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i]);
+        mover->Next=temp;
+        mover=temp;
+    }
+    return head;
+}
+
+int main(){
+    
+    vector<int> arr1 = {1,2,3,4,5};
+
+    Node* head = ArraytoLL(arr1);
+
+    cout<<head->data<<endl<<head->Next;
+
+}
