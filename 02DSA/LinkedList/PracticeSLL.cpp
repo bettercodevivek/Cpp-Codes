a1ac83760513c546aeabb22e3c7cc2c0672c430a
bettercodevivek <viveksingh0520@gmail.com>
2025-02-22 14:27:57 +0530

    Insert fucntion implemented

--- 02DSA/LinkedList/PracticeSLL.cpp
+++ 02DSA/LinkedList/PracticeSLL.cpp
@@ -14,6 +14,38 @@ class Node{
     }
 };
 
+class LinkedList {
+    private:
+    Node* head;
+
+    public:
+    LinkedList(){
+        head = nullptr;
+    }
+
+    void insertNode(int val){
+        Node* newNode = new Node(val);
+        if(head == nullptr){
+            head=newNode;
+            return;
+        }
+        Node* temp=head;
+        while(temp->Next != nullptr){
+            temp=temp->Next;
+        }
+        temp->Next=newNode;
+    }
+
+    void Display(){
+        Node* temp = head;
+        while(temp != nullptr){
+            cout<<temp->data<<"->";
+            temp=temp->Next;
+        }
+        cout<<"nullptr"<<endl;
+    }
+};
+
 Node* ArrayToLL(vector<int> &arr){
     Node* head = new Node(arr[0]);
     Node* mover = head;
@@ -27,24 +59,34 @@ Node* ArrayToLL(vector<int> &arr){
 }
 
 int main(){
+
+    LinkedList list1;
+
+    list1.insertNode(10);
+    list1.insertNode(20);
+    list1.insertNode(30);
+    list1.insertNode(40);
+    list1.insertNode(50);
+
+    list1.Display();
    
-    int n;
-    cin>>n;
-    vector<int> vec(n) ;
+    // int n;
+    // cin>>n;
+    // vector<int> vec(n) ;
    
 
-    for(int i=0;i<n;i++){
-        int x;
-        cin>>x;
-        vec[i]=x;
-    }
+    // for(int i=0;i<n;i++){
+    //     int x;
+    //     cin>>x;
+    //     vec[i]=x;
+    // }
     
-    Node* head = ArrayToLL(vec);
+    // Node* head = ArrayToLL(vec);
 
-    Node*temp = head;
+    // Node*temp = head;
 
-    while(temp != nullptr){
-        cout<<temp->data<<"->";
-        temp=temp->Next;
-    }
+    // while(temp != nullptr){
+    //     cout<<temp->data<<"->";
+    //     temp=temp->Next;
+    // }
 }
