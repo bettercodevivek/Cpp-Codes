41ad8bf3580c13c74a7a90fe9268f14cb07224b7
bettercodevivek <viveksingh0520@gmail.com>
2025-03-01 19:21:04 +0530

    Revising Linkedlist

---
+++ 02DSA/LinkedList/Practice.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* Next;
+
+    public:
+    Node(int value){
+        data=value;
+        Next=nullptr;
+    }
+};
+
+class LinkedList{
+    private:
+    Node* head;
+
+    public:
+    LinkedList(){
+        head=nullptr;
+    }
+   void insertAtEnd(int val){
+    Node* newnode = new Node(val);
+    if(head == nullptr){
+        head=newnode;
+        return;
+    }
+    Node* temp = head;
+    while(temp->Next != nullptr){
+        temp=temp->Next;
+    }
+    temp->Next=newnode;
+   }
+   void insertAtBegin(int val){
+    Node* newnode = new Node(val);
+    newnode->Next = head;
+    head=newnode;
+   }
+   void insertAtSpecific(int val, int pos){
+
+    // make sure to insert edge case for empty linked list and for index out of bounds
+      Node* newnode = new Node(val);
+      int idx = 0;
+      Node* temp = head;
+      while(temp != nullptr && idx < pos - 1){
+        idx++;
+        temp=temp->Next;
+      }
+      newnode->Next = temp->Next;
+      temp->Next=newnode;
+   }
+   void display(){
+    Node* temp = head;
+    while(temp != nullptr){
+        cout<<temp->data<<"->";
+        temp=temp->Next;
+    }
+    cout<<"NULL"<<endl;
+   }
+
+};
+
+int main(){
+    LinkedList list1;
+    list1.insertAtEnd(10);
+    list1.insertAtEnd(20);
+    list1.insertAtEnd(30);
+    list1.insertAtEnd(40);
+    list1.insertAtBegin(60);
+    list1.insertAtSpecific(70,6);
+
+    list1.display();
+}
